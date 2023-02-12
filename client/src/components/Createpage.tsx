771befa4370edec10bd088ba5dd3d4d837e8fb2d
Elmo Mittilä <celmi001@edu.xamk.fi>
2023-02-12 08:49:22 +0200

    Create operation done

--- client/src/components/Createpage.tsx
+++ client/src/components/Createpage.tsx
@@ -1,12 +1,16 @@
 import { Box, Button, TextField, Checkbox } from '@mui/material';
-import React, { useRef, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 import Grid from '@mui/material/Unstable_Grid2';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Typography from '@mui/material/Typography';
+import { MemberContext } from '../context/MemberContext';
+import { Member } from '../interfaces/interfaces'
 
 export const Createpage: React.FC = (): React.ReactElement => {
 
+
+    const { addMember } = useContext(MemberContext);
     const navigate: NavigateFunction = useNavigate();
     const formRef = useRef<HTMLFormElement>();
     const [error, setError] = useState<boolean>(false);
@@ -17,6 +21,18 @@ export const Createpage: React.FC = (): React.ReactElement => {
             formRef.current!.lastname.value &&
             formRef.current!.phone.value &&
             formRef.current!.email.value) {
+
+            const newMember: Member = {
+                id: 0,
+                firstname: formRef.current!.firstname.value,
+                lastname: formRef.current!.lastname.value,
+                phone: formRef.current!.phone.value,
+                email: formRef.current!.email.value,
+                active: formRef.current!.active.checked,
+                signedUpDate: new Date()
+            }
+
+            addMember(newMember);
             setError(false)
             navigate("/");
         } else {
